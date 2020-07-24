b637323f0d65f11e37eb6452ad855b0ed28c8662
Nikolai Schreier <nikolai.schreier@gmail.com>
2020-07-24 20:01:27 +0200

    Corrected start script for 1.15

--- minecraft/loop.sh
+++ minecraft/loop.sh
@@ -1,4 +1,4 @@
 while [ -e restart ]
 do
-  java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -Xmx8g -Xms8g -jar forge-1.14.4-28.2.16.jar
+  java -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -Xmx8g -Xms8g -jar forge-1.15.2-31.2.31.jar
 done
